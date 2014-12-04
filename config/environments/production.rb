2ffe9375a39dc70d7794f9fe8e5f29c45b315869
yaboyanees <a.merzi@yahoo.com>
2014-12-04 17:36:54 -0600

    updated production file for images to compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Dormmatch::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-	config.serve_static_assets = true
 	config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-	config.assets.compile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -22,14 +20,14 @@ Dormmatch::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
